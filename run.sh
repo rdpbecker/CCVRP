195b5f59cf7bc12dc03e58ace71e064de8e9d282
Riley <rileydbecker@gmail.com>
2020-03-12 11:10:27 -0400

    Change run to send an OS notification on completion instead of saying done

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@ if [ -z "$2" ]; then
     echo "Usage: ./run.sh <input file> <output number>"
     exit 1
 fi
-julia $1.jl > out$1$2; spd-say "Done"
+julia $1.jl > out$1$2; 
+notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "Done"
